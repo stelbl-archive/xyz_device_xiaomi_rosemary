7bad3e54b99c6a4cec3259a0f758632d74a51114
R0rt1z2 <rogerortizleal@gmail.com>
2022-08-09 22:04:58 +0100

    rosemary: Patch camera blob to increase maximum JPEG streams
    * Increase maxAppJpegStreamNum from 5 to 11 in order to fix GCam
      lags
    
    [LinkBoi00] Adapt for cannon and use classic sed instead of xxd+sed
    
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>
    Signed-off-by: fjrneedkanajo iyh <fajarslebew31@gmail.com>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -81,6 +81,9 @@ function blob_fixup() {
     vendor/etc/init/vendor.mediatek.hardware.mtkpower@1.0-service.rc)
         echo "$(cat ${2}) input" > "${2}"
         ;;
+    vendor/lib64/libmtkcam_featurepolicy.so)
+        sed -i "s|\xE8\x87\x40\xB9|\x28\x02\x80\x52|g" "${2}"
+        ;;
     esac
 }
 
