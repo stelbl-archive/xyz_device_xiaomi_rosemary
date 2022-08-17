6cd2720d9276391e51888845fb503ae1e2ce640d
bengris32 <bengris32@protonmail.ch>
2022-08-17 18:53:04 +0100

    rosemary/blobs: Patchelf C2 service to link against libavservices_minijail
    * The libavservices_minijail_vendor library was substituted for
      the vendor variant of libavservices_minijail in Android 13.
    
    * Patchelf the C2 service to link against libavservices_minijail
      instead of libavservices_minijail_vendor.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -87,6 +87,11 @@ function blob_fixup() {
    vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
         "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
         ;;
+   vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
+        ;&
+   vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
+        "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
+        ;;
     esac
 }
 
