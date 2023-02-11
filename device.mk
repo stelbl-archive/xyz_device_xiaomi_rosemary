0dcf04f13c4040d979f0d926130052f8d285d5da
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-02-11 03:30:48 +0100

    rosemary: Import public libraries from stock
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -106,6 +106,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
     $(LOCAL_PATH)/configs/perf/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml
 
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6785 \
