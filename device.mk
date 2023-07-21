489ffbdfa6c0a5d95d3582d5eb50dd3b64b74e4a
bengris32 <bengris32@protonmail.ch>
2023-07-21 13:10:34 +0100

    rosemary: Ship MediaTek cgroups configuration
    * MediaTek ship their own modified task_profiles and
      use the V30 cgroups configuration.
    
    Change-Id: I15245ea87b00925597db65f1564e808a094c937a
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -139,6 +139,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Cgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json 
+
 # Display
 PRODUCT_PACKAGES += \
     libdrm.vendor \
