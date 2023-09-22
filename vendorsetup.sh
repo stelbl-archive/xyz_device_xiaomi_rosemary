234c933a8d885878a947661be5a8ca5ce3c894a4
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-09-22 03:29:40 +0000

    rosemary: Add more clone required repos
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,9 @@ rm -rf device/mediatek/sepolicy_vndr
 
 echo "Cloning required repositories."
 git clone https://github.com/lineageos/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+git clone https://github.com/xiaomi-mt6781-devs/android_hardware_mediatek hardware/mediatek
+git clone ssh://git@github.com/whoisgabutuniverseshit/proprietary_vendor_xiaomi_rosemary vendor/xiaomi/rosemary
+git clone https://github.com/xiaomi-mt6785-dev/android_kernel_xiaomi_mt6785 kernel/xiaomi/rosemary --depth=1 --single-branch
 
 echo "Removing charger images on vendor."
 rm -rf vendor/*/charger
