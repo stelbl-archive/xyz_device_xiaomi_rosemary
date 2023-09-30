8eed18697d321a25d82dafaaa5a784ccc8fa4c21
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-09-30 21:31:46 +0000

    rosemary: Move-out overlay-lineage to overlay folder * Also enforce the rro targets
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -319,7 +319,11 @@ PRODUCT_PACKAGES += \
     WifiOverlayRosemary
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 # Partitions
