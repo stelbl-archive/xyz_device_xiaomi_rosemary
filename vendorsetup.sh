d98309d5d40ea47847206212b29639b6987fb3aa
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-09-30 15:09:29 +0000

    rosemary: Add more vendorsetup script
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,11 +8,17 @@ git clone https://github.com/lineageos/android_device_mediatek_sepolicy_vndr dev
 git clone https://github.com/xiaomi-mt6781-devs/android_hardware_mediatek hardware/mediatek
 git clone ssh://git@github.com/whoisgabutuniverseshit/proprietary_vendor_xiaomi_rosemary vendor/xiaomi/rosemary
 git clone https://github.com/xiaomi-mt6785-dev/android_kernel_xiaomi_mt6785 kernel/xiaomi/rosemary --depth=1 --single-branch
+git clone https://gitlab.com/xyzuniverse/android_vendor_xiaomi_rosemary-firmware vendor/xiaomi/rosemary-firmware
 
 echo "Removing charger images on vendor."
 rm -rf vendor/*/charger
 
+echo "Removing gnss on hardware/mediatek"
+cd hardware/mediatek
+rm -rf gnss*
+rm -rf */gnss*
+cd -
+
 echo "Setting up builder username and hostname."
 export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=xyzuniverse
-
