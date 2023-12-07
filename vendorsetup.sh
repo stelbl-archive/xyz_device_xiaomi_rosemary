b32fde6a76e96b40ecff503317305417069a33eb
Firmank123 <firmankediri123@gmail.com>
2023-12-07 13:13:39 +0000

    rosemary: vendorsetup: Adapt to mine
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,8 +6,8 @@ rm -rf device/mediatek/sepolicy_vndr
 echo "Cloning required repositories."
 git clone https://github.com/lineageos/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 git clone https://github.com/xiaomi-mt6781-devs/android_hardware_mediatek hardware/mediatek
-git clone ssh://git@github.com/whoisgabutuniverseshit/proprietary_vendor_xiaomi_rosemary vendor/xiaomi/rosemary
-git clone https://github.com/xiaomi-mt6785-dev/android_kernel_xiaomi_mt6785 kernel/xiaomi/rosemary --depth=1 --single-branch
+git clone git@github.com:ChuckProjekt/vendor_xiaomi_rosemaryp.git vendor/xiaomi/rosemary
+git clone -b inline https://github.com/silont-project/kernel_xiaomi_rosemary kernel/xiaomi/rosemary --depth=1 --single-branch
 git clone https://gitlab.com/xyzuniverse/android_vendor_xiaomi_rosemary-firmware vendor/xiaomi/rosemary-firmware
 
 echo "Removing charger images on vendor."
@@ -20,5 +20,5 @@ rm -rf */gnss*
 cd -
 
 echo "Setting up builder username and hostname."
-export BUILD_USERNAME=nobody
-export BUILD_HOSTNAME=xyzuniverse
+export BUILD_USERNAME=ChuckDXD
+export BUILD_HOSTNAME=GengKapak
