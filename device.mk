9b87598455019e87419a76a98e7d0426ddd282a4
bengris32 <bengris32@protonmail.ch>
2023-12-19 15:25:28 +0000

    rosemary: Move to common sensors service [Untested]
    Change-Id: Idebada879d5c64d59c2bc2ee06146f33b862538d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: tomyprs <tomiprs25@gmail.com>
    
    sepolicy: `file_context` remove duplicate affter pick commit
    
    Signed-off-by: tomyprs <tomiprs25@gmail.com>
    
    fix picked up remove unused sepolicy
    
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

--- device.mk
+++ device.mk
@@ -469,12 +469,15 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
 PRODUCT_PACKAGES += \
     init.recovery.mt6785.rc
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
