579807d5203a5b3bba95a35e8e7590e1339cf5b2
Radinka Putri El Wahyudi <147782260+clyfly@users.noreply.github.com>
2024-01-11 10:12:27 +0700

    rosemary: update tracking branch remote to clybuild repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@ rm -rf device/mediatek/sepolicy_vndr
 echo "Cloning required repositories."
 git clone https://github.com/lineageos/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 git clone https://github.com/xiaomi-mt6781-devs/android_hardware_mediatek hardware/mediatek
-git clone git@github.com:ChuckProjekt/vendor_xiaomi_rosemaryp.git vendor/xiaomi/rosemary
+git clone https://github.com/cly-build/vendor-xiaomi-rosemary vendor/xiaomi/rosemary
 git clone -b inline https://github.com/silont-project/kernel_xiaomi_rosemary kernel/xiaomi/rosemary --depth=1 --single-branch
 git clone https://gitlab.com/xyzuniverse/android_vendor_xiaomi_rosemary-firmware vendor/xiaomi/rosemary-firmware
 
@@ -20,5 +20,5 @@ rm -rf */gnss*
 cd -
 
 echo "Setting up builder username and hostname."
-export BUILD_USERNAME=ChuckDXD
-export BUILD_HOSTNAME=GengKapak
+export BUILD_USERNAME=rad
+export BUILD_HOSTNAME=lazyer
