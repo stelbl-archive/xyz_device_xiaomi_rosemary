0115df316378e97f47d8dbe941309698e3790cd4
Radinka Putri El Wahyudi <147782260+clyfly@users.noreply.github.com>
2024-01-25 14:24:17 +0700

    rosemary: dt

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_rosemary.mk
+    $(LOCAL_DIR)/rad_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_rosemary-user \
-    aosp_rosemary-userdebug \
-    aosp_rosemary-eng
+    rad_rosemary-user \
+    rad_rosemary-userdebug \
+    rad_rosemary-eng
