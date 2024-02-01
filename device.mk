f47845ec361fece66a82cffffbb40d8a6c4cfbd3
troublescope <121922135+troublescope@users.noreply.github.com>
2024-02-01 23:52:37 +0000

    rosemary: Add xiaomi Soong namespaces
    Signed-off-by: troublescope <121922135+troublescope@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -486,7 +486,8 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/mediatek
+    hardware/mediatek \
+    hardware/xiaomi
 
 # SoundTrigger
 PRODUCT_PACKAGES += \
